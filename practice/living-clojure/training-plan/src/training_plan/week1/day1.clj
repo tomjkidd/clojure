40e9043cacc8bd525b0860faa65b1f4e1a8a44a7
Tom Kidd <tomjkidd@gmail.com>
2016-01-28 13:52:12 -0500

    Updating Week1/Day1 to use clojure.test

--- practice/living-clojure/training-plan/src/training_plan/week1/day1.clj
+++ practice/living-clojure/training-plan/src/training_plan/week1/day1.clj
@@ -1,62 +1,47 @@
 (ns training-plan.week1.day1
-  (:require [clojure.set]))
+  (:require [clojure.set])
+  (:use [clojure.test]))
 
 ;; [Google Group](http://bit.ly/4clojure_group)
 
-(def introToStrings
-  (= "HELLO WORLD" (.toUpperCase "hello world")))
+(deftest introToStrings
+  (is (= "HELLO WORLD" (.toUpperCase "hello world"))))
 
-(def introToLists
-  (= (list :a :b :c) '(:a :b :c)))
+(deftest introToLists
+  (is (= (list :a :b :c) '(:a :b :c))))
 
-(def listsConj
-  (= '(1 2 3 4) 
-     (conj '(2 3 4) 1)
-     (conj '(3 4) 2 1)))
+(deftest listsConj
+  (is (= '(1 2 3 4) 
+         (conj '(2 3 4) 1)
+         (conj '(3 4) 2 1))))
 
-(def introToVectors
-  (= [:a :b :c]
-     (list :a :b :c)
-     (vec '(:a :b :c))
-     (vector :a :b :c)))
+(deftest introToVectors
+  (is (= [:a :b :c]
+         (list :a :b :c)
+         (vec '(:a :b :c))
+         (vector :a :b :c))))
 
-(def vectorsConj
-  (= [1 2 3 4] (conj [1 2 3] 4)
-     [1 2 3 4] (conj [1 2] 3 4)))
+(deftest vectorsConj
+  (is (= [1 2 3 4] (conj [1 2 3] 4)
+         [1 2 3 4] (conj [1 2] 3 4))))
 
-(def introToSets
-  (= #{:a :b :c :d} (set '(:a :a :b :c :c :c :c :d :d))
-     (clojure.set/union #{:a :b :c} #{:b :c :d})))
+(deftest introToSets
+  (is (= #{:a :b :c :d} (set '(:a :a :b :c :c :c :c :d :d))
+         (clojure.set/union #{:a :b :c} #{:b :c :d}))))
 
-(def setsConj
-  (= #{1 2 3 4} (conj #{1 4 3} 2)))
+(deftest setsConj
+  (is (= #{1 2 3 4} (conj #{1 4 3} 2))))
 
-(def introToMaps
-  (= 20 
-     ((hash-map :a 10 :b 20 :c 30) :b)
-     (:b {:a 10 :b 20 :c 30})))
+(deftest introToMaps
+  (is (= 20 
+         ((hash-map :a 10 :b 20 :c 30) :b)
+         (:b {:a 10 :b 20 :c 30}))))
 
-(def mapsConj
-  (= {:a 1 :b 2 :c 3} (conj {:a 1} [:b 2] [:c 3])))
+(deftest mapsConj
+  (is (= {:a 1 :b 2 :c 3} (conj {:a 1} [:b 2] [:c 3]))))
 
-(def introToSequences
-  (= 3 
-     (first '(3 2 1))
-     (second [2 3 4])
-     (last (list 1 2 3))))
-
-(def tests
-  [introToStrings
-   introToLists
-   listsConj
-   introToVectors
-   vectorsConj
-   introToSets
-   setsConj
-   introToMaps
-   mapsConj
-   introToSequences
-   ])
-
-(defn tests-pass []
-  (every? (fn [x] (= true x)) tests))
+(deftest introToSequences
+  (is (= 3 
+         (first '(3 2 1))
+         (second [2 3 4])
+         (last (list 1 2 3)))))
